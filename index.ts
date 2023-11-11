63bb5aa6ebe8b202565415ccfa89ce9d767c3783
Vũ Khương Duy <duy.vk@zinza.com.vn>
2023-11-11 16:30:46 +0000

    Transfer usdt

--- index.ts
+++ index.ts
@@ -1,25 +1,37 @@
-const main = () => {
+var ABI_JSON = require('../abis/erc20.json')
+var {Web3} = require('web3')
+
+const send = async () => {
+  var web3 = new Web3('wss://ethereum.publicnode.com')
+  // var web3 = new Web3('wss://ethereum-goerli.publicnode.com')
+
+  const usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+  // const usdtAddress = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984'
+
   const usdtContract = new web3.eth.Contract(ABI_JSON, usdtAddress);
+  const privateKey = '';
+  const gasPrice = await web3.eth.getGasPrice()
+  const tx = {
+    from: '',
+    to: usdtContract._address,
+    data: usdtContract.methods.transfer(toAddress, 18276791).encodeABI(),
+    value: '0x00',
+    gasPrice: gasPrice,
+    gasLimit: '0x33450',
+  }
+  
+  const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey)
+  console.log({signedTx})
 
-  return new Promise((resolve, reject) => {
-    web3.eth.getGasPrice()
-        .then(gasPrice => {
-            web3.eth.sendTransaction({
-                from: from,
-                to: usdtContract._address,
-                data: usdtContract.methods.transfer(to, web3.utils.toWei(amount, 'ether')).encodeABI(),
-            value: '0x00',
-            gasPrice: gasPrice,
-            gasLimit: '0x33450',
-        })
-        .then((receipt) => {
-            resolve(receipt)
-        })
-        .catch(e => reject(e))
-    })
-    .catch(e => reject(e))
+  const result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
 
-  }) 
+  return result
+    
+}
+
+const main = async () => {
+  const res = await send()
+  console.log({res})
 }
 
 main()
